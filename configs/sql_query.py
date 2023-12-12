bffc930d38593484f13de85a94d653d39f5df11a
nsharma28 <125905409+nsharma28@users.noreply.github.com>
2023-12-12 13:53:21 +0530

    Update sql_query.py

--- configs/sql_query.py
+++ configs/sql_query.py
@@ -5,3 +5,7 @@ __get_analytics_file.close()
 __property_file = open("queries/getpropertylist.sql", 'r')
 get_property_query = __property_file.read()
 __property_file.close()
+
+__property_detail_file = open("queries/getpropertydetail.sql", 'r')
+get_property_detail_query = __property_detail_file.read()
+__property_detail_file.close()

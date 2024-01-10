f5c09f45246d3d4c28eaa5f0ef972ef09624ec5e
nsharma28 <125905409+nsharma28@users.noreply.github.com>
2024-01-10 17:05:02 +0530

    Update sql_query.py

--- configs/sql_query.py
+++ configs/sql_query.py
@@ -9,3 +9,11 @@ __property_file.close()
 __property_detail_file = open("queries/getpropertydetail.sql", 'r')
 get_property_detail_query = __property_detail_file.read()
 __property_detail_file.close()
+
+_active_vs_sold_file = open("queries/getactivevssold.sql", "r")
+get_active_vs_sold_query = _active_vs_sold_file.read()
+_active_vs_sold_file.close()
+
+_status_wise_count_file = open("queries/getstatuswisecount.sql","r")
+get_status_wise_count_query = _status_wise_count_file.read()
+_status_wise_count_file.close()     
